a3c3792919e392a6d4e8ed047a139d577bc94590
mayankagarwal44442 <mayankagarwal44442@gmail.com>
2019-10-21 18:30:34 +0530

    finished implementation

--- tcp_client.py
+++ tcp_client.py
@@ -1,17 +1,47 @@
 from socket import *
-# serverName = input("Enter the serverName")
-# serverPort = int(input("Enter the port name"))
+from functools import reduce
 
 serverName = "127.0.0.1"
 serverPort = 12000
 
+def wrap_add(a, b):			# a and b are binary string, we have to do udp style wrap adding
+	c = bin(int(a,2) + int(b,2))[2:]			# add a and b in binary format and clip the intial '0b'
+	if(len(c) == 8):							# if overflow happened, wrap around
+		c = bin(int(c[1:],2) + int('1',2))		
+	return c[2:].zfill(7)						#make sure 7 bits and send back
+
+
+def compute_checksum(sentence):
+	binary_padded_equivalent = [format(ord(i), 'b').zfill(7) for i in sentence]
+	wrapped_result = reduce(wrap_add, binary_padded_equivalent)
+	return wrapped_result
+
+def complement(wrapped_value):
+	return ''.join(str(int(i,2)^1) for i in  wrapped_value) 
+
+def read_file(file_path):
+	fd = open(file_path, 'r')
+	content = fd.read()
+	fd.close()
+	return content
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-sentence = input("Input lowercase sentence:")
+file_path = input("Input absolute file path")
 
-clientSocket.send(sentence.encode())
-clientSocket.send((sentence+"bla").encode())
+content = read_file(file_path)
 
-modifiedSentence = clientSocket.recv(1024).decode()
-print('From Server:', modifiedSentence)
+checksum = complement(compute_checksum(content))
+
+packet = content + "\n" + checksum 		#last line will always be checksum
+
+clientSocket.send(packet.encode())
+
+acknowledgement_message = clientSocket.recv(1024).decode()		#ACK or NAK depending on whether message was received without corruption
+
+if(acknowledgement_message == "ACK"):
+	print("Message received by Sender")
+else:
+	print("Packet Loss, message was corrupted when received by Sender")
+
 clientSocket.close()
--- tcp_server.py
+++ tcp_server.py
@@ -1,19 +1,54 @@
 from socket import *
+from functools import reduce
+
+def wrap_add(a, b):			# a and b are binary string, we have to do udp style wrap adding
+	c = bin(int(a,2) + int(b,2))[2:]			# add a and b in binary format and clip the intial '0b'
+	if(len(c) == 8):							# if overflow happened, wrap around
+		c = bin(int(c[1:],2) + int('1',2))		
+	return c[2:].zfill(7)						#make sure 7 bits and send back
+
+
+def compute_checksum(sentence):
+	binary_padded_equivalent = [format(ord(i), 'b').zfill(7) for i in sentence]
+	wrapped_result = reduce(wrap_add, binary_padded_equivalent)
+	return wrapped_result
+
+
+def extract_checksum(sentence):
+	checksum = sentence.split("\n")[-1]
+	return checksum
+
+def extract_message(sentence):	
+	message = ''.join(sentence.split("\n")[:-1])
+	return message
+
+def validate_checksum(received_checksum, computed_checksum):
+	c = bin(int(received_checksum,2) + int(computed_checksum,2))[2:]
+	return c == '1111111'
+
 serverPort = 12000
+
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(("",serverPort))
 serverSocket.listen(1)
 print("The server is ready to receive")
 while 1:
 	connectionSocket, addr = serverSocket.accept()
-	
+
 	sentence = connectionSocket.recv(1024).decode()
-	capitalizedSentence = sentence.upper()
-	connectionSocket.send(capitalizedSentence.encode())
-	
-	sentence = connectionSocket.recv(1024).decode()
-	capitalizedSentence = sentence.upper()
-	connectionSocket.send(capitalizedSentence.encode())
+
+	received_checksum = extract_checksum(sentence)
+	print("Received checksum:", received_checksum)
+
+	message = extract_message(sentence)
+	computed_checksum = compute_checksum(message)
+
+	if(validate_checksum(received_checksum, computed_checksum)):
+		acknowledgement_message = "ACK"
+	else:
+		acknowledgement_message = "NAK"
+
+	connectionSocket.send(acknowledgement_message.encode())
 
 
 	connectionSocket.close()
